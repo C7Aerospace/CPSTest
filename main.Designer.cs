ae15c0fa4eb3f3edc4baafb08c21a2b6b93e124a
C7 <1374760231@qq.com>
2018-07-17 19:42:53 +0800

    Initial commit

---
+++ main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CPSTest
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sign = new System.Windows.Forms.Label();
+            this.cpsDisplay = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.clickButton = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // sign
+            // 
+            this.sign.AutoSize = true;
+            this.sign.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sign.ForeColor = System.Drawing.Color.White;
+            this.sign.Location = new System.Drawing.Point(0, 0);
+            this.sign.Margin = new System.Windows.Forms.Padding(0);
+            this.sign.Name = "sign";
+            this.sign.Size = new System.Drawing.Size(267, 17);
+            this.sign.TabIndex = 0;
+            this.sign.Text = "CPS(Click Per Second)测试程式  ver 0.1 by C7";
+            // 
+            // cpsDisplay
+            // 
+            this.cpsDisplay.AutoSize = true;
+            this.cpsDisplay.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cpsDisplay.ForeColor = System.Drawing.Color.Red;
+            this.cpsDisplay.Location = new System.Drawing.Point(0, 17);
+            this.cpsDisplay.Margin = new System.Windows.Forms.Padding(0);
+            this.cpsDisplay.Name = "cpsDisplay";
+            this.cpsDisplay.Size = new System.Drawing.Size(114, 17);
+            this.cpsDisplay.TabIndex = 1;
+            this.cpsDisplay.Text = "这里会显示CPS信息";
+            // 
+            // beginButton
+            // 
+            this.beginButton.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.beginButton.ForeColor = System.Drawing.Color.Black;
+            this.beginButton.Location = new System.Drawing.Point(112, 78);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(60, 36);
+            this.beginButton.TabIndex = 2;
+            this.beginButton.Text = "开始";
+            this.beginButton.UseVisualStyleBackColor = true;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // clickButton
+            // 
+            this.clickButton.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clickButton.ForeColor = System.Drawing.Color.Red;
+            this.clickButton.Location = new System.Drawing.Point(12, 38);
+            this.clickButton.Name = "clickButton";
+            this.clickButton.Size = new System.Drawing.Size(260, 116);
+            this.clickButton.TabIndex = 3;
+            this.clickButton.Text = "Click Me !!!";
+            this.clickButton.UseVisualStyleBackColor = true;
+            this.clickButton.Visible = false;
+            this.clickButton.Click += new System.EventHandler(this.clickButton_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ProgressBar.Location = new System.Drawing.Point(0, 160);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(284, 5);
+            this.ProgressBar.TabIndex = 4;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(284, 165);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.clickButton);
+            this.Controls.Add(this.cpsDisplay);
+            this.Controls.Add(this.sign);
+            this.Name = "main";
+            this.Text = "CPSTest";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sign;
+        private System.Windows.Forms.Label cpsDisplay;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.Button clickButton;
+        private System.Windows.Forms.Panel ProgressBar;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CPSTest
+{
+    public partial class main : Form
+    {
+        public List<DateTime> clickTime = new List<DateTime>();
+        Thread thread1;
+        public main()
+        {
+            InitializeComponent();
+        }
+        public void stop()
+        {
+            beginButton.Visible = true;
+            clickButton.Visible = false;
+            thread1 = null;
+            showCPSInfo();
+            clickTime = new List<DateTime>();
+        }
+        private void showCPSInfo()
+        {
+            long[] distanceTimeArray = new long[clickTime.Count-1];
+            double[] CPSData = new Double[clickTime.Count-1];
+            
+            for (int i = 1; i < clickTime.Count; i++)
+            {
+                distanceTimeArray[i-1] = clickTime[i].ToBinary()-clickTime[i-1].ToBinary();
+            }
+            for (int i = 0; i < distanceTimeArray.Length; i++)
+            {
+                CPSData[i] = 10000000d / distanceTimeArray[i];
+            }
+            double max = CPSData[0], min = CPSData[0];
+            for (int i = 0; i < CPSData.Length; i++)
+            {
+                if(CPSData[i]>max)
+                    max = CPSData[i];
+                if (CPSData[i] < min)
+                    min = CPSData[i];
+            }
+            double avg = (double)clickTime.Count/10d;
+            cpsDisplay.Text = String.Format("CPS信息: 平均:{0} 峰值:{1} 谷值:{2}", avg.ToString("00.0"), max.ToString("00.00"), min.ToString("00.00"));
+        }
+        private void clickButton_Click(object sender, EventArgs e)
+        {
+            clickTime.Add(DateTime.Now);
+        }
+
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            beginButton.Visible = false;
+            clickButton.Visible = true;
+            thread1 = new Thread(() => {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(10);
+                    ProgressBar.Width = (int)(284 * ((double)(i + 1) / (double)1000));
+                }
+                stop();
+            });
+            thread1.Start();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
